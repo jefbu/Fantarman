2b334c9da58bff6e1f21a31a781a1b4ebf38fd61
jefb <jefb@HI89441.cegekanv.corp.local>
2017-10-02 13:28:17 +0200

    Tactic Selection Popup v0.1

--- src/main/graphics/gameScreen/regimentView/captainDetail/orderPanel/IndividualOrderPanel.java
+++ src/main/graphics/gameScreen/regimentView/captainDetail/orderPanel/IndividualOrderPanel.java
@@ -1,9 +1,13 @@
 package main.graphics.gameScreen.regimentView.captainDetail.orderPanel;
 
 import java.awt.Dimension;
+import java.awt.MouseInfo;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 
+import main.components.Colour;
 import main.utility.Colors;
 
 public class IndividualOrderPanel extends JButton {
@@ -16,6 +20,17 @@ public class IndividualOrderPanel extends JButton {
 		setPreferredSize(new Dimension(width, height));
 		setBorder(null);
 		setBackground(Colors.lred);
+		
+		addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				OrderChoicePopup orderChoicePopup = new OrderChoicePopup(width * 2, height * 10, Colour.DRED, false);
+				orderChoicePopup.setLocation(MouseInfo.getPointerInfo().getLocation());
+				int x = orderChoicePopup.getX();
+				int y = orderChoicePopup.getY() - height * 10;
+				orderChoicePopup.setLocation(x, y);
+				orderChoicePopup.setVisible(true);
+			}
+		});
 		
 		
 		
---
+++ src/main/graphics/gameScreen/regimentView/captainDetail/orderPanel/OrderChoicePopupOrderPanel.java
@@ -0,0 +1,15 @@
+package main.graphics.gameScreen.regimentView.captainDetail.orderPanel;
+
+import javax.swing.JPanel;
+
+public class OrderChoicePopupOrderPanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	public OrderChoicePopupOrderPanel() {
+		
+		super();
+		
+	}
+
+}
