6d396d0a57bb58bad2647165b57d6fee12ecc720
jefb <jefb@HI89441.cegekanv.corp.local>
2017-10-12 10:13:59 +0200

    commit fix

--- src/main/graphics/gameScreen/regimentView/captainDetail/OrderPanel.java
+++ src/main/graphics/gameScreen/regimentView/captainDetail/OrderPanel.java
@@ -58,6 +58,7 @@ public class OrderPanel extends ContentPanel {
 					orderChoicePopup.setLocation(x, y);
 					orderChoicePopup.setVisible(true);
 					orderList.get(index).removeActionListener(orderList.get(index).getActionListeners()[0]);
+					System.out.println("commit fix");
 				}
 		});
 		
