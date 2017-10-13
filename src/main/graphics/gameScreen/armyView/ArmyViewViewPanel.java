8df6773c090fa2079dbc04752110e57e3ab4b484
jefb <jefb@HI89441.cegekanv.corp.local>
2017-10-13 14:29:07 +0200

    Fixed army view button size

--- src/main/graphics/gameScreen/armyView/ArmyViewViewPanel.java
+++ src/main/graphics/gameScreen/armyView/ArmyViewViewPanel.java
@@ -2,6 +2,7 @@ package main.graphics.gameScreen.armyView;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.MouseInfo;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -21,7 +22,9 @@ public class ArmyViewViewPanel extends JPanel {
 		super();
 		setPreferredSize(new Dimension(width, height));
 		setBackground(new Color (150, 170, 255));
+		setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
 		sortButton = new JButton();
+		sortButton.setPreferredSize(new Dimension(width, height));
 		sortButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				ViewChoicePopup viewChoicePopup = new ViewChoicePopup(width, height * 4 + 10, Colour.DGREEN, false);
