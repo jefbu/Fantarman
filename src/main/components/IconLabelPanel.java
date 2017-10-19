e172b8a84afd84ae572e4003a68a80ec6d2623d3
jefb <jefb@HI89441.cegekanv.corp.local>
2017-10-19 09:52:12 +0200

    Title Screen Buttons

--- src/main/components/IconLabelPanel.java
+++ src/main/components/IconLabelPanel.java
@@ -14,10 +14,14 @@ public class IconLabelPanel extends JPanel {
 	public JLabel iconLabel;
 	public ButtonedPanel buttonedPanel;
 	ImageLoader imageLoader;
+	private int width;
+	private int height;
 	
 	public IconLabelPanel(int width, int height, Colour colour) {
 		
 		super();
+		this.width = width;
+		this.height = height;
 		setPreferredSize(new Dimension(width, height));
 		setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
 		
@@ -25,8 +29,7 @@ public class IconLabelPanel extends JPanel {
 		
 		iconLabel = new JLabel();
 		iconLabel.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
-		iconLabel.setIcon(imageLoader.loadImageIcon("/components/IconLabelPanelBlue.png", width, height));
-		
+		decideIcon(colour);		
 		add(iconLabel);
 		
 		buttonedPanel = new ButtonedPanel(width, height);
@@ -36,6 +39,32 @@ public class IconLabelPanel extends JPanel {
 
 		iconLabel.add(buttonedPanel);
 		
+		
+	}
+	
+	private void decideIcon(Colour colour) {
+		switch(colour) {
+		case BLUE: iconLabel.setIcon(imageLoader.loadImageIcon("/components/IconLabelPanelBlue.png", width, height));
+			break;
+		case DBLUE: iconLabel.setIcon(imageLoader.loadImageIcon("/components/IconLabelPanelBlue.png", width, height));
+			break;
+		case DGREEN: iconLabel.setIcon(imageLoader.loadImageIcon("/components/IconLabelPanelGreen.png", width, height));
+			break;
+		case DRED: iconLabel.setIcon(imageLoader.loadImageIcon("/components/IconLabelPanelRed.png", width, height));
+			break;
+		case GREEN: iconLabel.setIcon(imageLoader.loadImageIcon("/components/IconLabelPanelGreen.png", width, height));
+			break;
+		case LBLUE: iconLabel.setIcon(imageLoader.loadImageIcon("/components/IconLabelPanelBlue.png", width, height));
+			break;
+		case LGREEN: iconLabel.setIcon(imageLoader.loadImageIcon("/components/IconLabelPanelGreen.png", width, height));
+			break;
+		case LRED: iconLabel.setIcon(imageLoader.loadImageIcon("/components/IconLabelPanelRed.png", width, height));
+			break;
+		case RED: iconLabel.setIcon(imageLoader.loadImageIcon("/components/IconLabelPanelRed.png", width, height));
+			break;
+		default:
+			break;	
+		}
 		
 	}
 
--- src/main/graphics/gameScreen/TitleScreen.java
+++ src/main/graphics/gameScreen/TitleScreen.java
@@ -1,15 +1,22 @@
 package main.graphics.gameScreen;
 
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import main.Main;
 import main.components.Colour;
 import main.components.IconLabelPanel;
+import main.graphics.Screen;
+import main.utility.Colors;
 import main.utility.ImageLoader;
 
 public class TitleScreen extends JPanel {
@@ -26,12 +33,12 @@ public class TitleScreen extends JPanel {
 		
 		ImageLoader imageLoader = new ImageLoader();
 		try {
-			image = imageLoader.loadBufferedImage("/components/IconLabelPanelRed.png");
+			image = imageLoader.loadBufferedImage("/components/TitleBackground.png");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-        JPanel panel = new JPanel() {
+        JPanel backgroundPanel = new JPanel() {
 			private static final long serialVersionUID = 1L;
 			@Override
             protected void paintComponent(Graphics g) {
@@ -39,11 +46,49 @@ public class TitleScreen extends JPanel {
                 g.drawImage(image, 0, 0, width, height, null);
             }
         };
-        panel.setPreferredSize(new Dimension(width, height));
-        add(panel);
+        backgroundPanel.setPreferredSize(new Dimension(width, height));
+        backgroundPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+        add(backgroundPanel);
         
-        IconLabelPanel startPanel = new IconLabelPanel(width / 3, height / 5, Colour.BLUE);
-        panel.add(startPanel);
+        JPanel topPanel = new JPanel();
+        topPanel.setPreferredSize(new Dimension(width, height / 10));
+        topPanel.setOpaque(false);
+        backgroundPanel.add(topPanel);
+        
+        JPanel middlePanel = new JPanel();
+        middlePanel.setPreferredSize(new Dimension (width, height / 10 * 8));
+        middlePanel.setOpaque(false);
+        middlePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 20));
+        backgroundPanel.add(middlePanel);
+        
+        IconLabelPanel startPanel = new IconLabelPanel(width / 2, height / 5, Colour.BLUE);
+        startPanel.buttonedPanel.label.setForeground(Colors.textyellow);
+        startPanel.buttonedPanel.label.setFont(new Font("garamond", Font.BOLD, 48));
+        startPanel.buttonedPanel.label.setText("Start");
+        startPanel.buttonedPanel.button.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		Screen.titleScreen.setVisible(false);
+        		Screen.gameScreen.setVisible(true);
+        	}
+        });
+        middlePanel.add(startPanel);
+        
+        IconLabelPanel loadPanel = new IconLabelPanel(width / 2, height / 5, Colour.GREEN);
+        loadPanel.buttonedPanel.label.setForeground(Colors.textyellow);
+        loadPanel.buttonedPanel.label.setFont(new Font("garamond", Font.BOLD, 48));
+        loadPanel.buttonedPanel.label.setText("Load");
+        middlePanel.add(loadPanel);
+        
+        IconLabelPanel exitPanel = new IconLabelPanel(width / 2, height / 5, Colour.RED);
+        exitPanel.buttonedPanel.label.setForeground(Colors.textyellow);
+        exitPanel.buttonedPanel.label.setFont(new Font("garamond", Font.BOLD, 48));
+        exitPanel.buttonedPanel.label.setText("Quit");
+        exitPanel.buttonedPanel.button.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		Main.screen.dispose();
+        	}
+        });
+        middlePanel.add(exitPanel);
 
 		
 	}
