90304eb4ea5ca1f0aab4363ce14bff05e0df7002
jefbu <Jefbussens@gmail.com>
2018-02-24 20:47:31 +0100

    started working on lieutenants; game recognises how many a regiment gets

--- src/main/graphics/gameScreen/regimentView/LieutenantsDetailPanel.java
+++ src/main/graphics/gameScreen/regimentView/LieutenantsDetailPanel.java
@@ -13,6 +13,10 @@ import main.utility.Colors;
 public class LieutenantsDetailPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	
+	LieutenantDetailPanel lieutenantDetailPanel1;
+	LieutenantDetailPanel lieutenantDetailPanel2;
+	LieutenantDetailPanel lieutenantDetailPanel3;
 
 	public LieutenantsDetailPanel(int width, int height) {
 
@@ -21,11 +25,11 @@ public class LieutenantsDetailPanel extends JPanel {
 		setBackground(Colors.white);
 		setLayout(new FlowLayout(FlowLayout.LEFT, 10, 5));
 
-		LieutenantDetailPanel lieutenantDetailPanel1 = new LieutenantDetailPanel(width / 4 - 5, height - 10, Colour.LBLUE,
+		lieutenantDetailPanel1 = new LieutenantDetailPanel(width / 4 - 5, height - 10, Colour.LBLUE,
 				new FlowLayout(FlowLayout.LEFT, 5, 5));
-		LieutenantDetailPanel lieutenantDetailPanel2 = new LieutenantDetailPanel(width / 4 - 5, height - 10, Colour.LBLUE,
+		lieutenantDetailPanel2 = new LieutenantDetailPanel(width / 4 - 5, height - 10, Colour.LBLUE,
 				new FlowLayout(FlowLayout.LEFT, 5, 5));
-		LieutenantDetailPanel lieutenantDetailPanel3 = new LieutenantDetailPanel(width / 4 - 5, height - 10, Colour.LBLUE,
+		lieutenantDetailPanel3 = new LieutenantDetailPanel(width / 4 - 5, height - 10, Colour.LBLUE,
 				new FlowLayout(FlowLayout.LEFT, 5, 5));
 
 		add(lieutenantDetailPanel1);
@@ -36,6 +40,9 @@ public class LieutenantsDetailPanel extends JPanel {
 	
 	public void fillLieutenantsDetailPanel(Regiment regiment) {
 		
+		lieutenantDetailPanel1.fillLieutenantDetailPanel(1, regiment);
+		lieutenantDetailPanel2.fillLieutenantDetailPanel(2, regiment);
+		lieutenantDetailPanel3.fillLieutenantDetailPanel(3, regiment);		
 		
 	}
 
--- src/main/graphics/gameScreen/regimentView/lieutenantDetail/LieutenantDetailPanel.java
+++ src/main/graphics/gameScreen/regimentView/lieutenantDetail/LieutenantDetailPanel.java
@@ -6,23 +6,38 @@ import javax.swing.JLabel;
 
 import main.components.Colour;
 import main.components.ContentPanel;
+import main.entity.regiments.Regiment;
 import main.utility.ImageLoader;
 
 public class LieutenantDetailPanel extends ContentPanel {
 
 	private static final long serialVersionUID = 1L;
 	private ImageLoader imageLoader;
+	public JLabel portrait;
+	private int width;
+	private int height;
 
 	public LieutenantDetailPanel(int width, int height, Colour colour, FlowLayout flowLayout) {
 		
 		super(width, height, colour, flowLayout);
 		imageLoader = new ImageLoader();
 		
-		JLabel portrait = new JLabel();
-		portrait.setIcon(imageLoader.loadImageIcon("/tiles/grass3.png", width / 4, height - 20));
+		this.width = width;
+		this.height = height;
+		
+		portrait = new JLabel();
 		insidePanel.add(portrait);
 		
-
+	}
+	
+	public void fillLieutenantDetailPanel(int order, Regiment regiment) {
+		
+		if (order > regiment.lieutenantsSize) {
+		portrait.setIcon(imageLoader.loadImageIcon("/lieutenants/none.png", width / 4, height - 25));
+		}
+		else {
+		portrait.setIcon(imageLoader.loadImageIcon("/lieutenants/mendicant.png", width / 4, height - 25));			
+		}
 		
 	}
 	
