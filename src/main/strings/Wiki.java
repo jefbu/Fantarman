f063d73fb8f922967679d203167a0d6fc63486ba
jefbu <Jefbussens@gmail.com>
2018-06-21 19:19:31 +0200

    v0.5.6.1. Adapted encyclopedia
    - Tightened the encyclopedia for the alpha test

--- src/main/strings/Wiki.java
+++ src/main/strings/Wiki.java
@@ -1,55 +1,37 @@
 package main.strings;
 
 public abstract class Wiki {
-	
-	public static String combatString = "<font face = 'arial' color = 'rgb(250, 250, 175)'>" +
-	"Part 1: Basic Combat <br> <br>" +
-	"<font color = 'rgb(250, 230, 230)'>" +
-	"Every battle, a random <font color = 'rgb(200, 200, 250)'> map <font color = 'rgb(250, 230, 230)'> " + 
-	"will be generated. The battle will start with a deployment phase, " +
-	"after which the battle proper plays out. The battle will take several turns, during which every regiment will " +
-	"get at least one chance to act. <br>" +
-	"Regiments will try to carry out the orders that you gave them, unless of course if they're in combat, in which case " +
-	"they will be far too occupied in melee. <br>" +
-	"The battle ends after the last turn (which will be slightly randomised but always between 6 and 8), or if one army " +
-	"has been completely annihilated. <br>" +
-	"The most important thing to remember is that you, the player, will have only very limited control of how the battle " +
-	"plays out. Just like a real general, you can give your regiments orders at the outset" + 
-	", but as soon as things get moving, they are on their own. <br>" +
-	"As such; the main gameplay in Hans Off My Army comes from preparing your plans and setting up your regiments' " +
-	"orders accordingly. In a later version, minor interventions during the battle will be added to the game." +
-	"<br> <br> <font color = 'rgb(250, 250, 175)'>" +
-	"Part 2: The Map <br>" + 
-	"<font color = 'rgb(250, 230, 230)'> The Map is randomly generated for every battle. Exciting landscapes full of hills"
-	+ ", deserts, rivers and forests keep every battle a unique challenge of its own. Apart from the aesthetics; "
-	+ "the map does actually serves its purpose. Every tile is designated a certain type of terrain. And every"
-	+ "type of terrain has an impact on how easy it is to move over it. The map itself contains 48 by 32 tiles."
-	+ "<br> In a later stage, terrain will also affect defensive capacities of <font color = 'rgb(200, 200, 250)'>"
-	+ " regiments <font color  = 'rgb(250, 230, 230)'>, and will have a height that will impact lines of sight. <br>";
-	
-	
-	
-	
-	public static String managementString = "<font face = 'arial' color = 'rgb(200, 180, 255)'>" +
-	"Oi";
-	
-	
-	
-	public static String conceptsString = "<font face = 'arial' color = 'rgb(250, 250, 175)'>" +
-	"Attack: <font color = 'rgb(250, 230, 230)'> The percentage chance of a soldier's attack to be successful. " +
-	"During a melee, the active regiment will test their remaining life points times against this percentage. " +
-	"For each successful roll, the defender will have to check on their " +
-	"<font color = 'rgb(200, 200, 250)'> defence <font color = 'rgb(250, 230, 230)'> stat to try and prevent the damage"; 
-	
-	
-	
-	
-	public static String fluffString = "<font face = 'arial' color = 'rgb(200, 220, 255)'>" +
-	"blnain";
-	
-	
-	
-	public static String creditsString = "<font face = 'arial' color = 'rgb(250, 250, 175)'>" +
-	"A Game by MyCompany";
+
+	public static String combatString = "<font face = 'arial' color = 'rgb(250, 250, 175)'>"
+			+ "Part 1: Basic Combat <br> <br>" + "<font color = 'rgb(250, 230, 230)'>"
+			+ "In Homa, you have no direct control over the battle. The regiments will attempt to perform their orders <br> "
+			+ "(as given during the management stage), starting with the first and moving down."
+			+ "The battle lasts between 6 and 8 turns. During every turn, every regiment gets a chance to act at least once, <br>"
+			+ "sometimes twice depending on their speed. Regiments in combat will be locked in melee until this is resolved, <br>"
+			+ "after which they will continue carrying out their orders as before." + "<br> <br>"
+			+ "<font color = 'rgb(250, 250, 175)'>" + "Part 2: The Map <br> <br> "
+			+ "<font color = 'rgb(250, 230, 230)'>"
+			+ "For every battle, a new map is randomly generated. Every tile in the map contains a certain terrain, "
+			+ "which decides how hard it is to move over.";
+
+	public static String managementString = "<font face = 'arial' color = 'rgb(250, 230, 230)'>"
+			+ "The most important screen is the army overview, where you can see your regiments and assign them roles. <br>"
+			+ "You can assign eight regiments into a combat role, these will be the ones to participate in battle. <br>"
+			+ "Clicking on a specific regiment will bring you to its overview screen, where you can change some equipment options,"
+			+ " and most of all, assign the regiment's orders." + "<br> <br>"
+			+ "These orders follow a format of condition - target - order. During the battle, every time the regiment gets its turn,"
+			+ "it will attempt to carry out an order. It starts with the topmost order (some regiments have more intelligent"
+			+ " captains and as such get more orders), checks the condition and if this one is valid, "
+			+ "attempts to carry out the order, aimed at the chosen target.";
+
+	public static String conceptsString = "<font face = 'arial' color = 'rgb(250, 250, 175)'>"
+			+ "Attack: <font color = 'rgb(250, 230, 230)'> The percentage chance of a soldier's attack to be successful. "
+			+ "During a melee, the active regiment will test their remaining life points times against this percentage. "
+			+ "For each successful roll, the defender will have to check on their "
+			+ "<font color = 'rgb(200, 200, 250)'> defence <font color = 'rgb(250, 230, 230)'> stat to try and prevent the damage";
+
+	public static String fluffString = "<font face = 'arial' color = 'rgb(250, 230, 230)'>" + "Nothing here yet.";
+
+	public static String creditsString = "<font face = 'arial' color = 'rgb(250, 230, 230)'>" + "A Game by Ekster Games";
 
 }
