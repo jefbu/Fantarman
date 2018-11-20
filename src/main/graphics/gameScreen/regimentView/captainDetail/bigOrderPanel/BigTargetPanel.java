779b4712df71994b6a71ce26f6beedeb8a0b8494
jefbu <Jefbussens@gmail.com>
2018-11-20 18:52:33 +0100

    v0.6.7.0. Final Version for 20 Nov

--- src/main/graphics/gameScreen/regimentView/captainDetail/bigOrderPanel/BigTargetPanel.java
+++ src/main/graphics/gameScreen/regimentView/captainDetail/bigOrderPanel/BigTargetPanel.java
@@ -3,7 +3,9 @@ package main.graphics.gameScreen.regimentView.captainDetail.bigOrderPanel;
 import javax.swing.JPanel;
 
 public class BigTargetPanel extends JPanel {
-	
+
+	private static final long serialVersionUID = 1L;
+
 	public BigTargetPanel() {
 		
 		super();
